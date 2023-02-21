3401651b0417bb056fca91fdaf2931855e987908
Sven Lilge <slilge@cs.toronto.edu>
2023-02-21 11:52:40 -0500

    Removed wrong include statements

--- include/fbgs_interface.h
+++ include/fbgs_interface.h
@@ -5,21 +5,6 @@ Copyright (C) 2022 Sven Lilge, Continuum Robotics Laboratory, University of Toro
 
 #pragma once
 
-#include <Eigen/Core>
-#include <vector>
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <cmath>
-#include <array>
-
-
-#include "cosseratrodmodel.h"
-#include "constantcurvaturemodel.h"
-#include "piecewiseconstantcurvaturemodel.h"
-#include "pseudorigidbodymodel.h"
-#include "subsegmentcosseratrodmodel.h"
-
 
 // This class implements a simple interface to the FBGS sensing system utilizing TCP sockets
 class FBGSInterface
