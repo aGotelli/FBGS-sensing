005014d3abbaeffea0839f1ffecd1ebb43257d00
Sven Lilge <slilge@cs.toronto.edu>
2023-02-21 18:29:02 -0500

    Working on a first interface to the shape sensing software

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ Copyright (C) 2022 Sven Lilge, Continuum Robotics Laboratory, University of Toro
 
 //includes
 #include <iostream>
-
+#include <vector>
+#include <string>
 #include <Eigen/Dense>
 
 
@@ -17,6 +18,20 @@ Copyright (C) 2022 Sven Lilge, Continuum Robotics Laboratory, University of Toro
 //defines
 #define SERVER_ADDRESS "192.168.1.11"
 #define PORT_NUMBER "5001"
+
+std::string string_to_hex(const std::string& input)
+{
+    static const char hex_digits[] = "0123456789abcdef";
+
+    std::string output;
+    output.reserve(input.length() * 2);
+    for (unsigned char c : input)
+    {
+        output.push_back(hex_digits[c >> 4]);
+        output.push_back(hex_digits[c & 15]);
+    }
+    return output;
+}
 
 int main(int argc, char **argv)
 {
@@ -34,7 +49,7 @@ int main(int argc, char **argv)
 		boost::asio::ip::tcp::endpoint ep = *endpoints;
 		std::cout << ep << std::endl;
 		
-		std::cout << "Establishing connection to socket... "; 
+		std::cout << "Establishing connection... "; 
 		
 		//Connect to socket and open connection
 		boost::asio::ip::tcp::socket socket(io_context);
@@ -47,23 +62,67 @@ int main(int argc, char **argv)
 		//Run in an endless loop and display received data
 		while(1)
 		{
-			boost::array<char, 128> buf;
-			boost::system::error_code error;
-
-			size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-			if (error == boost::asio::error::eof)
+			//boost::array<char, 128> buf;
+			//boost::system::error_code error;
+			//
+			//size_t len = socket.read_some(boost::asio::buffer(buf), error);
+			//
+			//if (error == boost::asio::error::eof)
+			//{
+			//	std::cout << "Connection closed by server." << std::endl << std::endl;
+			//	break; // Connection closed cleanly by peer.
+			//}
+			//else if (error)
+			//{
+			//	std::cout << "Error occured!" << std::endl << std::endl;
+			//	throw boost::system::system_error(error); // Some other error.
+			//}
+			//
+			//std::cout << buf.data() << std::endl << std::endl;
+			
+			//Check if we can read data without blocking
+			if(socket.available() > 0)
 			{
-				std::cout << "Connection closed by server." << std::endl << std::endl;
-				break; // Connection closed cleanly by peer.
+			
+				boost::asio::streambuf data;
+				
+				//First read the first 4 bytes to figure out the size of the following ASCII string
+				size_t size_read = boost::asio::read(socket,data,boost::asio::transfer_exactly(4));
+				
+				std::cout << size_read << std::endl;
+				
+				std::string data_string;
+				std::istream is(&data);
+				is >> data_string;
+				
+				std::string hex_string = string_to_hex(data_string);
+				
+				std::cout << hex_string << std::endl;
+				int data_length = std::stoi(hex_string,0,16);
+				std::cout << data_length << std::endl;
+			
+				
+				//Now read the remaining ASCII string of the current data package
+				size_read = boost::asio::read(socket,data,boost::asio::transfer_exactly(data_length));
+				
+				std::cout << size_read << std::endl;
+				
+				data_string.clear();
+				std::istream is2(&data);;
+				
+				is2.peek(); // try state of stream
+				while(is2.good())  
+				{
+					getline(is2,data_string,  '\t');     // FBGS uses tab as delimiter
+					std::cout << data_string << std::endl;
+					is2.peek(); // set eof flag if end of data is reached
+				}
+				
+				break;
 			}
-			else if (error)
-			{
-				std::cout << "Error occured!" << std::endl << std::endl;
-				throw boost::system::system_error(error); // Some other error.
-			}
-
-			std::cout << buf.data() << std::endl << std::endl;
+			
+			
+    
 		}
 				
 	}
